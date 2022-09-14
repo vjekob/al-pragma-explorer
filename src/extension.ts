44030b72bca15ee7ae37d20f53eab67f5c7d239f
Matthias Rabus <matthias.rabus@biteam.de>
2022-09-14 11:04:33 +0200

    handle view visibility

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Extension "al-pragma-explorer" is now active!');
+    
+    vscode.commands.executeCommand("setContext", "al-pragma-explorer.showPragmaExplorer", true);
 }
 
 export function deactivate() {}
