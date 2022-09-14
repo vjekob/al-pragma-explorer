b1226b7717eaf132b02288013b0483835190116b
Valeri Bojarski <v.bojarski@hensel-recycling.com>
2022-09-14 10:26:33 +0200

    formated ALObjectType

--- src/model/ALObjectType.ts
+++ src/model/ALObjectType.ts
@@ -1,8 +1,7 @@
-    
 export enum ALObjectType {
-        "Page"=1,
-        "Table"=2,
-        "Query"=3,
-        "Codeunit"=4,
-        "Report"=5
+    'Page' = 1,
+    'Table' = 2,
+    'Query' = 3,
+    'Codeunit' = 4,
+    'Report' = 5,
 }
